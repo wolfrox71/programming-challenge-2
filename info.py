080897a2144bcfe11a04420e906df549a518cebe
jr113396 <32139738+wolfrox71@users.noreply.github.com>
2021-11-16 09:13:24 +0000

    Initial commit

---
+++ info.py
@@ -0,0 +1,7 @@
+#login
+allow_blank_passwords = False #allows the user to have a password consisting of just spaces
+
+#create user
+defualt_role = "user"
+roles=["user","admin"]
+admin_roles=["admin"]
